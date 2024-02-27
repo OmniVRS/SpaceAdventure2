a4023690377eb974622dda6e2915df6c6b551f51
SveltestWorm65 <wormgamedevelopment@gmail.com>
2024-02-27 08:24:08 -0800

    imported all old scripts

---
+++ Assets/Isaiah's Folder/Scripts/HazardScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardScript : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = spawnPoint.transform.position;
+        }
+    }
+}

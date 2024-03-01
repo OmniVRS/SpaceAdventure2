158ebd844e6aba0c151d48d4ea94fb7fc1dd3e62
OmniVRS <scottkimball01@gmail.com>
2024-03-01 10:14:21 -0800

    Fixed Scanner

--- Assets/Scott/Scripts/ScannerGadget.cs
+++ Assets/Scott/Scripts/ScannerGadget.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 public class ScannerGadget : MonoBehaviour
 {
     public bool scanOn;
-    public GameObject[] secrets;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        scanOn = false;
-        secrets = GameObject.FindGameObjectsWithTag("Secret");
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            foreach (GameObject secret in secrets)
+            ToggleScan();
+        }
+    }
+
+    void ToggleScan()
+    {
+        scanOn = !scanOn; // Toggle scanOn flag
+
+        GameObject[] secrets = GameObject.FindGameObjectsWithTag("Secret");
+
+        foreach (GameObject secret in secrets)
+        {
+            SpriteRenderer secretRenderer = secret.GetComponent<SpriteRenderer>();
+            if (secretRenderer != null)
             {
-                if (!scanOn)
-                {
-                    scanOn = true;
-                    SpriteRenderer secretsColor = secret.GetComponent<SpriteRenderer>();
-                    secret.GetComponent<SpriteRenderer>().color = new Color(secretsColor.color.r, secretsColor.color.g, secretsColor.color.b, 0.5f);
-                }
-                else if (scanOn)
-                {
-                    scanOn = false;
-                    SpriteRenderer secretsColor = secret.GetComponent<SpriteRenderer>();
-                    secret.GetComponent<SpriteRenderer>().color = new Color(secretsColor.color.r, secretsColor.color.g, secretsColor.color.b, 0f);
-                }
+                Color color = secretRenderer.color;
+                color.a = scanOn ? 0.5f : 0f; // Set alpha based on scanOn
+                secretRenderer.color = color;
+            }
+            else
+            {
+                Debug.LogWarning("Secret GameObject is missing SpriteRenderer component.");
             }
         }
     }

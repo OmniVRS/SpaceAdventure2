d80db2e42105d28d5b05fd54c2e8f112b74336a1
SveltestWorm65 <wormgamedevelopment@gmail.com>
2024-03-05 09:21:24 -0800

    Credits finished

--- Assets/Isaiah's Folder/Scripts/Credits.cs
+++ Assets/Isaiah's Folder/Scripts/Credits.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour
+
+public class Credits : MonoBehaviour
 {
     public string nextLevel;
-    public void OnPlayButton ()
+    public void OnPlayButton()
     {
         SceneManager.LoadScene(nextLevel);
 
-    }
-    public void OnApplicationQuit()
-    {
-        Application.Quit();
     }
 }

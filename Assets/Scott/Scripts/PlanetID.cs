b0f1593df95ed136749a0e10f83f5cd01cb2fb1e
OmniVRS <scottkimball01@gmail.com>
2024-03-08 09:39:33 -0800

    Starmap updatef

--- Assets/Scott/Scripts/PlanetID.cs
+++ Assets/Scott/Scripts/PlanetID.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlanetID : MonoBehaviour
 {
     public string sendToScene;
+    public bool hasPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +17,23 @@ public class PlanetID : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && hasPlayer)
+        {
+            //Debug.Log("Clicked!");
+            SceneManager.LoadScene(sendToScene);
+        }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                SceneManager.LoadScene(sendToScene);
-            }
+            hasPlayer = true;  
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        hasPlayer = false;
     }
 }
